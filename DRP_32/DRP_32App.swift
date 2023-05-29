c4e2fe217e3122df2a0d2e121975f76f65c44976
Jiang, Boyuan <boyuan.jiang21@imperial.ac.uk>
2023-05-29 16:19:18 +0100

    Initial Commit

---
+++ DRP_32/DRP_32App.swift
@@ -0,0 +1,17 @@
+//
+//  DRP_32App.swift
+//  DRP_32
+//
+//  Created by 蒋伯源 on 2023/5/29.
+//
+
+import SwiftUI
+
+@main
+struct DRP_32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
