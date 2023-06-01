293ca26ae3c069e1a6b67ad8d82f79cfb3e0bcb7
jx1921 <jx1921@ic.ac.uk>
2023-06-01 11:39:03 +0100

    Add tap view color

--- DRP_32/Views/BottomNavigator/BottomNavigator.swift
+++ DRP_32/Views/BottomNavigator/BottomNavigator.swift
@@ -8,27 +8,42 @@
 import SwiftUI
 
 struct BottomNavigator: View {
-    @State private var selectedTab = 0
+    
+    @State private var selection: Tab = .routine
+
+    enum Tab {
+        case routine
+        case community
+        case question
+    }
+    
+    let tabBarColor = UIColor(red: 0.1, green: 0.1, blue: 0.3, alpha: 1.0)
+  
+    
+    init() {
+           UITabBar.appearance().backgroundColor = tabBarColor
+    }
     
     var body: some View {
-            TabView(selection: $selectedTab) {
+            TabView(selection: $selection) {
                 CalendarView()
                     .tabItem {
                         Label("Routine", systemImage: "calendar")
                     }
-                    .tag(0)
+                    .tag(Tab.routine)
                 
                 CommunityView()
                     .tabItem {
                         Label("Community", systemImage: "person.2")
                     }
-                    .tag(1)
+                    .tag(Tab.community)
+                
                 
                 QuestionView()
                     .tabItem {
                         Label("Questions", systemImage: "questionmark.bubble")
                     }
-                    .tag(2)
+                    .tag(Tab.question)
             }
         }
 }
