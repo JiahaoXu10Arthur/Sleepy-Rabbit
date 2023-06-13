a327b434afdeef86965612716a57375e915141ca
rl1921 <rl1921@ic.ac.uk>
2023-06-13 16:35:46 +0100

    Designing GetStartView

--- DRP_32/Views/OnboardView/GetStartView.swift
+++ DRP_32/Views/OnboardView/GetStartView.swift
@@ -11,8 +11,19 @@ struct GetStartView: View {
     @EnvironmentObject var settings: UserSettings
     var body: some View {
         VStack {
+          
+          Spacer()
+          Text("Eveything is ready")
+            .font(.system(size: 44, weight: .semibold))
+            .padding(.bottom)
+          
+          Text("Let's jump into the rabbit hole!")
+              .padding()
+          
             StartButtonView()
                 .padding(.top, 20)
+          
+          Spacer()
         } //: BUTTON
     }
 }
